654ecdace50f9453512b3f170f32f1e4ff458a24
hsingying <1103137212@gm.kuas.edu.tw>
2017-06-23 13:32:56 +0800

    加入專案檔案。

---
+++ 105-Final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_105_Final.Startup))]
+namespace _105_Final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
